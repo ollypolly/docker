ad1cab808d6a688b46d85a952cfefadbd21f40a0
Olly Pethick <ollypethick@gmail.com>
2024-03-13 12:02:29 +0000

    Add music project file to docker launch script

--- docker_launch.sh
+++ docker_launch.sh
@@ -115,6 +115,7 @@ sudo docker-compose \
     -f $DOCKER_COMPOSE_PATH/meta.yml \
     -f $DOCKER_COMPOSE_PATH/minecraft.yml \
     -f $DOCKER_COMPOSE_PATH/misc.yml \
+    -f $DOCKER_COMPOSE_PATH/music.yml \
     -f $DOCKER_COMPOSE_PATH/networking.yml \
     -f $DOCKER_COMPOSE_PATH/shows-movies.yml \
     up -d --remove-orphans 
