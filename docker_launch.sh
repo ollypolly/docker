881935ac3ebfcba84636c2945959fa4dd31ce058
Olly Pethick <ollypethick@gmail.com>
2024-08-28 17:55:07 +0100

    Add download client to docker stack

--- docker_launch.sh
+++ docker_launch.sh
@@ -111,19 +111,13 @@ create_folder "$DOCKER_PATH/speedtest-tracker"
 
 # Tautulli
 create_folder "$DOCKER_PATH/tautulli"
-
-# -f $DOCKER_COMPOSE_PATH/books-comics.yml \
-    
-    # -f $DOCKER_COMPOSE_PATH/minecraft.yml \
-    # -f $DOCKER_COMPOSE_PATH/misc.yml \
-    # -f $DOCKER_COMPOSE_PATH/monitoring.yml \
-    # -f $DOCKER_COMPOSE_PATH/music.yml \
     
 # Run docker compose on all files in docker-compose-files as one command
 sudo docker compose \
     -f $DOCKER_COMPOSE_PATH/shows-movies.yml \
     -f $DOCKER_COMPOSE_PATH/meta.yml \
     -f $DOCKER_COMPOSE_PATH/networking.yml \
+    -f $DOCKER_COMPOSE_PATH/dl-clients.yml \
     up -d --remove-orphans 
 
 
